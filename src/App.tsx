9a0a646b53b6e68c31d9c326327fa28edbb3c0fa
Nathan De Graaf <ndegraaf@iastate.edu>
2018-08-11 17:20:29 -0700

    first circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-
-import Button from '@material-ui/core/Button';
 import * as React from 'react';
+import {Circle, Layer, Stage, Text} from 'react-konva';
 import './App.css';
-import logo from './logo.svg';
+
 
 class App extends React.Component {
   public render() {
+    const height = window.innerHeight - 500;
+    const radius = (height - 100) / 2;
+    const centerX = window.innerWidth / 2;
+    const centerY = height / 2;
+
+    const textX = centerX - radius + 140;
+    const textY = centerY - 15;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to nedg.io</h1>
         </header>
-        <p className="App-intro">
-          The rest is coming soon...
-        </p>
-          <Button variant="contained" color="primary">
-              Hello World
-          </Button>
+        <Stage width={window.innerWidth} height={height}>
+          <Layer>
+            <Circle
+              x={centerX}
+              y={centerY}
+              radius={radius}
+              fill={"#f9ffad"}
+              stroke="black"
+            />
+            <Text
+              text="Cool stuff goes here"
+              fontSize={30}
+              align={"center"}
+              fontFamily={"Roboto"}
+              x={textX}
+              y={textY}
+            />
+          </Layer>
+        </Stage>
       </div>
     );
   }
